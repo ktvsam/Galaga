e1dfed3bf7de0d6adb6fd0bd1eb4ded3bc86d9ce
Twiggothy <152219043+Twiggothy@users.noreply.github.com>
2023-11-28 14:19:43 -0600

    Runner

---
+++ Galaga.java
@@ -0,0 +1,26 @@
+import javax.swing.JFrame;
+
+public class Galaga extends JFrame
+{
+	private static final int WIDTH = 1748; //1728
+	private static final int HEIGHT = 1364; //1344
+
+	public Galaga()
+	{
+		super("Galaga");
+
+		setSize(WIDTH,HEIGHT);
+
+
+		getContentPane().add(new Game() );
+
+		setVisible(true);
+	
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static void main( String args[] )
+	{
+		Galaga run = new Galaga();
+	}
+}
