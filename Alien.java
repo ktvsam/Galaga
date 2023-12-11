93b3aadbd31563ef21591b65780cf33d9cf1c755
James Dai <152219482+JDai12@users.noreply.github.com>
2023-12-11 14:20:46 -0600

    Alien.java

--- Alien.java
+++ Alien.java
@@ -6,29 +6,14 @@ import java.awt.event.*;
 import javax.swing.*;
 
 
-/*
-* Directions:
-*          Make the brick extend Block
-*
-*          Write the Brick constructor that takes an x, y, w, h to create a block.
-*
-*          Override the paint method to draw a Brick
-
-
-           Choose to draw or import an image.
-*
-*          Go to BreakOut and do Step 3, then test in the runner.
-*
-*          If all works correctly then move on to Steps 4 - 6 in BreakOut.
-*/
-//https://youtu.be/AheaTd_l5Is?t=152
 class Alien extends Canvas {
 
 
     private int x, y, w, h, startx, starty, yhelp, xsp, ysp;
+    private boolean loop;
+    private double degrees;
 
-
-    public Alien(int ex, int wy, int wd, int ht, int sp) {
+    public Alien(int ex, int wy, int wd, int ht, int sp, double dg) {
         x = ex;
         y = wy;
         w = wd;
@@ -38,32 +23,29 @@ class Alien extends Canvas {
         startx = 100;
         starty = 0;
         yhelp = 1;
+        degrees = 10;
+        loop = false;
     }
 
-//    void applyRotation(double angleInDegrees) {
-//        // Create a rotation transformation
-//        AffineTransform rotation = AffineTransform.getRotateInstance(
-//                Math.toRadians(angleInDegrees), x , y );
-//
-//
-//        // Apply the rotation to the coordinates of the alien
-//        Point2D rotatedPoint = rotation.transform(new Point2D.Double(x, y), null);
-//        setX((int)rotatedPoint.getX());
-//        setY((int)rotatedPoint.getY());
-//    }
-//    public void enter(){
-//        System.out.println("BEFORE ANGLE:" + getX() + " " + getY());
-//        applyRotation(90);
-//        System.out.println("BEFORE:" + getX() + " " + getY());
-//        x+=xsp;
-//        y+=ysp;
-//        System.out.println("AFTER: " + getX() + " " + getY());
-//    }
+    public void enter(){
+        System.out.println("BEFORE:" + getX() + " " + getY());
+        y+=ysp;
+        System.out.println("AFTER: " + getX() + " " + getY());
+    }
     public void paintRotate( Graphics window){
         Graphics2D g2 = (Graphics2D) window;
         Image img1 = Toolkit.getDefaultToolkit().getImage("david.png"); //use .gif or .png, you can choose the image
-        AffineTransform rotation = AffineTransform.getRotateInstance(Math.toRadians(15), x , y );
+//        AffineTransform rotation = AffineTransform.rotate(Math.toRadians(degrees),getX(),getY());
+        AffineTransform rotation = new AffineTransform();
+        rotation.translate(getX(), getY());
+        rotation.rotate(Math.toRadians(degrees));
+        rotation.scale(.55, .55);
+        System.out.println(getX() + " " + getY());
         g2.drawImage(img1, rotation, this);
+        degrees+=10;
+        if(degrees == 360){
+            setLoop(true);
+        }
     }
 
     public void paint( Graphics window )
@@ -73,41 +55,38 @@ class Alien extends Canvas {
         g2.drawImage(img1, getX(), getY(), getW(), getH(), this);
 
     }
-    public void enterUpLeftLine(){
+    // public void enterUpLeftLine(){
+    //     int slope = (800 - getY()) / (500 - getX());
+    //     int yint = (slope)*(getX()) - getY();
+    //     setX(getX()-xsp);
+    //     setY(slope*getX()-yint);
+    //     System.out.println(getX());
+    //     System.out.println(getY());
+    // }
+    // public void enterDownLeftLine(){
 
-        int slope = (800 - getY()) / (500 - getX());
-        int yint = (slope)*(getX()) - getY();
-        setX(getX()-xsp);
-        setY(slope*getX()-yint);
-        System.out.println(getX());
-        System.out.println(getY());
-    }
-    public void enterDownLeftLine(){
+    //     int slope = (800 - getY()) / (500 - getX());
+    //     int yint = (slope)*(getX()) + getY();
+    //     setX(getX()+xsp);
+    //     setY(slope*getX()+yint);
+    //     System.out.println(getX());
+    //     System.out.println(getY());
+    // }
+    // public void enterLeftLoop(){
 
-        int slope = (800 - getY()) / (500 - getX());
-        int yint = (slope)*(getX()) + getY();
-        setX(getX()+xsp);
-        setY(slope*getX()+yint);
-        System.out.println(getX());
-        System.out.println(getY());
-    }
-    public void enterLeftLoop(){
-
-    }
+    // }
     public int getX( ){ return x; }
     public void setX( int ex ){ x = ex; }
     public int getY( ){ return y; }
     public void setY( int wy ){ y = wy; }
     public int getW(){ return w; }
     public int getH(){ return h; }
-
-
-
-    public void movement1(){
-        this.setY(getY() + ysp*(int)Math.sin(30));
-
-
+    public void setLoop( boolean bo ){ loop = bo; }
+    public boolean getLoop(){ return loop; }
+    public Rectangle getRec() {
+        return new Rectangle(h, w);
     }
+
     public boolean intersects( Alien other )
     {
         Rectangle block = new Rectangle(x, y, w, h);
